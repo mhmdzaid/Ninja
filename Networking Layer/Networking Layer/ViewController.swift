1092266cf47c15452d62b6d95f00710999260e54
mozead1996 <mohamedzead2021@gmail.com>
2021-03-11 23:06:18 +0200

    removing fake data

--- Networking Layer/Networking Layer/ViewController.swift
+++ Networking Layer/Networking Layer/ViewController.swift
@@ -11,18 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let params :[String:Any] = ["first_name":"Ahmed",
-                      "last_name" : "Kamal",
-                      "phone" : "50733441",
-                      "image":UIImage(named: "AvatarMaker")!]
-        NinjaRequest.profile(params).upload(ProfileUploadResponse.self) { (result) in
-            switch result{
-            case .success(let response):
-                print("---------- success ------- \n \(response)")
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
+       
     }
 
 
